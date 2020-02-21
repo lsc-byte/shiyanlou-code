6ef4e492227c2a15dbe006efac59cfc416b72dae
lsc <123@123.com>
2020-02-21 21:59:25 +0800

    daima

---
+++ jump7.py
@@ -0,0 +1,14 @@
+shiyanlou:~/ $ cd /home/shiyanlou/Code                               [21:47:59]
+shiyanlou:Code/ $ git clone http://github.com/lsc-byte/shiyanlou-code.git
+正克隆到 'shiyanlou-code'...
+warning: 您似乎克隆了一个空仓库。
+检查连接... 完成。
+shiyanlou:Code/ $ ls                                                 [21:49:49]
+shiyanlou-code
+shiyanlou:Code/ $ cd shiyanlou-code                                  [21:50:03]
+shiyanlou:shiyanlou-code/ (master) $                                 [21:50:19]
+shiyanlou:shiyanlou-code/ (master) $                                 [21:55:36]
+
+
+
+
